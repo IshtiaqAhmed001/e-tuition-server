7d3a5eadee8b930282b067f3df498cd08e5e95b3
Ishtiaq <ishtiaq.wasi@gmail.com>
2025-12-08 00:49:33 +1100

    added post api for users

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nycnjuh.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -17,15 +21,26 @@ const client = new MongoClient(uri, {
   },
 });
 
+const eTuitionsDB = client.db("eTuitionsBD");
+const usersCollection = eTuitionsDB.collection("users");
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+     const query ={email:newUser.email}
+
+     const userExist = await usersCollection.findOne(query);
+     if(userExist){
+        return res.send({message:'user already exists!'})
+     }
+
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
