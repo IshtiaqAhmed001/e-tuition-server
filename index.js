f63c276c7a6ba2b251c63e579c04f71baf04d9ca
Ishtiaq <ishtiaq.wasi@gmail.com>
2025-12-09 01:05:51 +1100

    added user get api

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // users related api
+
+    app.get('/users/:email',async(req,res)=>{
+      const email = req.params.email;
+      const query ={email}
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+
+    })
     app.post("/users", async (req, res) => {
       const newUser = req.body;
       const query = { email: newUser.email };
@@ -45,9 +53,17 @@ async function run() {
 
     // tutors related api
     app.get("/tutors", async (req, res) => {
-      const cursor =  tutorsCollection.find({});
+      const cursor = tutorsCollection.find({});
       const result = await cursor.toArray();
       res.send(result);
+    });
+    app.get("/topTutors", async (req, res) => {
+      const topTutors = await tutorsCollection
+        .find()
+        .sort({ joinDate: -1 })
+        .limit(6)
+        .toArray();
+      res.send(topTutors);
     });
   } finally {
     // Ensures that the client will close when you finish/error
