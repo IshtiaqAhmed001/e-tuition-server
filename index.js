6bd3406d8fee8781d51d996d87c0ec269df72523
Ishtiaq <ishtiaq.wasi2@gmail.com>
2025-12-20 22:06:59 +1100

    fixed vercel and git email issue

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const cors = require("cors");
 const admin = require("firebase-admin");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
-const serviceAccount = require("./e-tuition-bd-firebase-adminsdk.json");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+  "utf8"
+);
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
--- key_converted.js
+++ key_converted.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
-const key = fs.readFileSync("./e-tuition-bd-firebase-adminsdk.json", "utf8");
+const key = fs.readFileSync("./e-tuition-bd-firebase-adminsdk.json.json", "utf8");
 const base64 = Buffer.from(key).toString("base64");
 console.log(base64);
+;
